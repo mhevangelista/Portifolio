044b09cc29a22a13e97179a31ef45e1b87608c58
mhevangelista <matheusevangelistanune@gmail.com>
2023-02-25 02:13:59 -0300

    Aprendendo a Usar Fetch (JavaScript)

---
+++ Fetch (Blog)/js/main.js
@@ -0,0 +1,46 @@
+const url = "https://jsonplaceholder.typicode.com/posts"
+
+const loadingElement = document.querySelector("#loadding")
+const postsContainer = document.querySelector("#posts-container")
+
+// Get id From URL 
+const urlSearchParams = new URLSearchParams(window.location.search)
+const postId = urlSearchParams.get("id")
+
+// Get All Post
+async function getAllPosts(){
+    const response = await fetch(url);
+
+    console.log(response)
+
+    const data = await response.json();
+    
+    console.log(data)
+
+    loadingElement.classList.add("hide")
+    
+    data.map((post) => {
+        const div = document.createElement('div');
+        const title = document.createElement('h2');
+        const body = document.createElement("p");
+        const link = document.createElement("a");
+
+        title.innerText = post.title;
+        body.innerText = post.body;
+        link.innerText = "Ler";
+        link.setAttribute("href", `/post.html?id=${post.id}`);
+
+        div.appendChild(title);
+        div.appendChild(body);
+        div.appendChild(link)
+
+        postsContainer.appendChild(div)
+    }) 
+}
+
+if(!postId){
+    getAllPosts()
+}else{
+    console.log(postId)
+}
+
