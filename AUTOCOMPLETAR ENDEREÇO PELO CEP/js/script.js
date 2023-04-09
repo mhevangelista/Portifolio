a3c2107088262f62b6d149d3303a573f8fc55716
mhevangelista <matheusevangelistanune@gmail.com>
2023-04-09 01:51:56 -0300

    atualizações

--- AUTOCOMPLETAR ENDEREÇO PELO CEP/js/script.js
+++ AUTOCOMPLETAR ENDEREÇO PELO CEP/js/script.js
@@ -5,17 +5,66 @@ const cityInput = document.querySelector("#city");
 const neighboordhoodInput = document.querySelector("#nheiborhood");
 const regionIput = document.querySelector("#region");
 const formInputs = document.querySelectorAll("[data-input]");
-
+const fadeElement = document.querySelector("#fade");
+const loaderElement = document.querySelector("#loader");
 closeButton = document.querySelector("#close-message");
 
 cepInput.addEventListener("keypress", (e) => {
     const onlyNumbers = /[0-9]/
     const key = String.fromCharCode(e.keyCode);
 
-    if(!onlyNumbers.test(key)){
+    if (!onlyNumbers.test(key)){
         e.preventDefault();
         return;
+    }
+});
+
+// Get address event 
+cepInput.addEventListener("keyup", (e) => {
+    const inputValue = e.target.value;
+
+    //Check if we have the correct legth
+    if (inputValue.length === 8){
+        getAddress(inputValue);
     }
     
 });
 
+// Get customer Address From API
+
+const getAddress = async (cep) => {
+    toggleLoader();
+    cepInput.blur();
+
+    const apiUrl = `https://viacep.com.br/ws/${cep}/json/`
+
+    const respose = await fetch(apiUrl)
+
+    const data = await respose.json()
+
+    if(data.erro === "true"){
+        addressForm.reset()
+        
+        toggleMensage("CEP Invalido tente Novamente")
+        return; 
+    }
+
+};
+
+const toggleLoader = () => {
+
+    fadeElement.classList.toggle("hide");
+    loaderElement.classList.toggle("hide")
+}
+
+const toggleMensage = (msg) => {
+
+    const messageElement = document.querySelector("#message")
+
+    const messageElementText = document.querySelector("#message p")
+
+    messageElementText.innerText = msg;
+
+    fadeElement.classList.toggle("hide")
+    messageElement.classList.toggle("hide")
+}
